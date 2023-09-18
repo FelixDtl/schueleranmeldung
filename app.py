f84249093c370671601ce174fe895df775f32619
maggi517 <m.dzinovic@gmx.de>
2023-09-18 09:49:44 +0200

    sessions

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, session, redirect, request
+from flask_session import Session
+import redis
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "redis" # null, filesystem, redis, cookie ...
+Session(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():  # put application's code here
+    if request.method == 'POST':
+        eingabe = request.form.get('start[typ]')
+        # Speichern Sie die Eingabedaten in der Sitzung
+        session['eingabe'] = eingabe
+        return redirect('/ausbildung')
     return render_template('index.html')
 
 
-@app.route('/ausbildung', methods=["POST"])
+
+@app.route('/ausbildung')
 def ausbildung():  # put application's code here
-    return render_template('ausbildung.html')
+        print(session['eingabe'])
+        # Abrufen der in der Sitzung gespeicherten Daten
+        eingabe = session.get('eingabe', 'Keine Eingabe vorhanden')
+        return render_template('ausbildung.html', eingabe=eingabe)
 
-@app.route('/zweiteSeite')
-def zweiteSeite():
-    return render_template('zweiteSeite.html')
-
-# Konstruktorfunktion
 if __name__ == '__main__':
     app.run()
